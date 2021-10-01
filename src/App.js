07184b44d9ec4e6f422c31890947441f9bc8d13c
devin <dhartman@sealworks.com>
2021-10-01 17:20:49 +0200

    Initial Setup Parcel, PostCss, React, Babel, Normalize

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { render } from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
